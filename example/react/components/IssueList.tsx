4103a4485cb3a0d885f581aba1e13eff4c057f9a
André "MG" Wisén <andre.wisen@gmail.com>
2023-07-06 23:15:34 +0200

    Add `IssueItem`

--- example/react/components/IssueList.tsx
+++ example/react/components/IssueList.tsx
@@ -1,7 +1,25 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
+import Button from '@mui/material/Button';
+
+import BCFViewer from '../../viewer/BCFViewer';
 import { RootState } from '../state/store';
 import { Topic } from '../../../src/core/three/dev';
+
+type IssueItemProps = {
+    topic: Topic;
+    children: React.ReactNode;
+};
+function IssueItem({ children, topic }: IssueItemProps) {
+    const onClick = () => {
+        BCFViewer.setTopicCameraState(topic);
+    };
+    return (
+        <Button variant="text" onClick={onClick}>
+            {children}
+        </Button>
+    );
+}
 
 export default function IssueList() {
     const topics = useSelector((state: RootState) => state.bcf.topics);
@@ -9,7 +27,12 @@ export default function IssueList() {
     const topicList = topics.map((input) => {
         const topic = new Topic();
         topic.fromJSON(input);
-        return <div key={topic.uuid}>{topic.direction}</div>;
+
+        return (
+            <IssueItem key={topic.uuid} topic={topic}>
+                {topic.direction}
+            </IssueItem>
+        );
     });
 
     return <div>{topicList}</div>;
