b3edc323c1d5b88da52bc986898a2d0d53953504
André "MG" Wisén <andre.wisen@gmail.com>
2023-07-08 23:11:27 +0200

    Add `issueDetails`

--- example/react/components/IssueList.tsx
+++ example/react/components/IssueList.tsx
@@ -1,21 +1,27 @@
 import React from 'react';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import Button from '@mui/material/Button';
 
 import BCFViewer from '../../viewer/BCFViewer';
 import { RootState } from '../state/store';
 import { Topic } from '../../../src/core/three/dev';
+import { SxProps } from '@mui/material';
+import { selectTopic } from '../state/bcfSlice';
 
 type IssueItemProps = {
+    isSelected?: boolean;
     topic: Topic;
     children: React.ReactNode;
 };
-function IssueItem({ children, topic }: IssueItemProps) {
+function IssueItem({ children, topic, isSelected }: IssueItemProps) {
+    const dispatch = useDispatch();
     const onClick = () => {
         BCFViewer.setTopicCameraState(topic);
+
+        dispatch(selectTopic(topic.order));
     };
     return (
-        <Button variant="text" onClick={onClick}>
+        <Button variant={isSelected ? 'outlined' : 'text'} onClick={onClick}>
             {children}
         </Button>
     );
@@ -23,13 +29,15 @@ function IssueItem({ children, topic }: IssueItemProps) {
 
 export default function IssueList() {
     const topics = useSelector((state: RootState) => state.bcf.topics);
+    const selected = useSelector((state: RootState) => state.bcf.selectedTopic);
 
     const topicList = topics.map((input) => {
         const topic = new Topic();
         topic.fromJSON(input);
+        const { uuid } = topic;
 
         return (
-            <IssueItem key={topic.uuid} topic={topic}>
+            <IssueItem key={uuid} topic={topic} isSelected={selected?.uuid === uuid}>
                 {topic.direction}
             </IssueItem>
         );
