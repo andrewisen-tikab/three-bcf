d098912138e305cb50c76bde7b7d08919b97c8fb
André "MG" Wisén <andre.wisen@gmail.com>
2023-07-09 21:23:55 +0200

    lint

---
+++ src/worker/utils.ts
@@ -0,0 +1,17 @@
+export const dataURLtoBlob = (dataURL: string) => {
+    const arr = dataURL.split(',');
+    const data = arr[0];
+    if (data) throw new Error('Invalid dataURL');
+    const match = data.match(/:(.*?);/);
+    if (!match) throw new Error('Invalid dataURL');
+    const mime = match[1];
+    const bstr = atob(arr[1]);
+    let n = bstr.length;
+    const u8arr = new Uint8Array(n);
+
+    while (n--) {
+        u8arr[n] = bstr.charCodeAt(n);
+    }
+
+    return new Blob([u8arr], { type: mime });
+};
--- src/worker/zip.ts
+++ src/worker/zip.ts
@@ -1,25 +1,10 @@
 import JSZip from 'jszip';
 import BCFVersion_XML from '../core/xml/bcf/root/bcf.version';
-import imgData from '../core/xml/bcf/topic/snapshot.png';
 
-import axios from 'axios';
 import Markup_XML from '../core/xml/bcf/topic/markup';
 import Viewpoint_XML from '../core/xml/bcf/topic/viewpoint';
 import { WorkerEventPostMessageData } from '../types';
-
-const dataURLtoBlob = (dataURL: string) => {
-    const arr = dataURL.split(',');
-    const mime = arr[0].match(/:(.*?);/)[1];
-    const bstr = atob(arr[1]);
-    let n = bstr.length;
-    const u8arr = new Uint8Array(n);
-
-    while (n--) {
-        u8arr[n] = bstr.charCodeAt(n);
-    }
-
-    return new Blob([u8arr], { type: mime });
-};
+import { dataURLtoBlob } from './utils';
 
 /**
  * @returns {Promise<Blob>} A promise that resolves to a Blob containing the BCF zip file
