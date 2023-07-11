23effc0918897d6aea52e36a803aed72048de2fe
André "MG" Wisén <andre.wisen@gmail.com>
2023-07-11 16:03:08 +0200

    Add `zod`

--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,7 @@ import Worker from './worker/worker?worker';
 import XMLCreator from './core/xml/xml';
 import BCF_XML from './core/xml/bcf/BCF';
 import Topic_XML from './core/xml/bcf/topic/topic';
-export { Worker, XMLCreator, BCF_XML as BCF, Topic_XML as Topic };
+import type { Topic } from './core/bcf/topic';
+import TopicSchema from './core/bcf/topic';
+
+export { Worker, XMLCreator, BCF_XML, Topic_XML, Topic, TopicSchema };
