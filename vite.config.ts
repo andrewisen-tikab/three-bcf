a3e64ed8cd02af8962191b30bfac4998c5ea3b87
André "MG" Wisén <andre.wisen@gmail.com>
2023-07-13 11:40:08 +0200

    Add `sourcemap`

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import dts from 'vite-plugin-dts';
 export default defineConfig({
     publicDir: false,
     build: {
+        sourcemap: true,
         lib: {
             // Could also be a dictionary or array of multiple entry points
             entry: resolve(__dirname, 'src/index.ts'),
