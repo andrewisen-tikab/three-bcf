7c37a10d2208a2c3878541369013621bc5674c93
André "MG" Wisén <andre.wisen@gmail.com>
2023-09-10 20:41:55 +0200

    Add `DataGrid`

--- example/react/components/IssueList.tsx
+++ example/react/components/IssueList.tsx
@@ -1,46 +1,58 @@
 import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import Button from '@mui/material/Button';
+
+import { DataGrid, GridColDef } from '@mui/x-data-grid';
+import { styled } from '@mui/material/styles';
 
 import BCFViewer from '../../viewer/BCFViewer';
 import { RootState } from '../state/store';
 import { selectTopic } from '../state/bcfSlice';
 import * as BCF from '../../../src';
 
-type IssueItemProps = {
-    isSelected?: boolean;
-    topic: BCF.THREE.Topic_Three;
-    children: React.ReactNode;
-};
-function IssueItem({ children, topic, isSelected }: IssueItemProps) {
-    const dispatch = useDispatch();
-    const onClick = () => {
-        BCFViewer.setTopicCameraState(topic);
+const columns: GridColDef[] = [
+    { field: 'index', headerName: 'No.', type: 'number', width: 80 },
+    { field: 'title', headerName: 'Title', width: 200 },
+    { field: 'description', headerName: 'Description', flex: 1 },
+];
 
-        dispatch(selectTopic(topic.index));
-    };
-    return (
-        <Button variant={isSelected ? 'outlined' : 'text'} onClick={onClick}>
-            {children}
-        </Button>
-    );
-}
+const StyledDataGrid = styled(DataGrid)(({ theme }) => ({
+    '.selected-row': { backgroundColor: theme.palette.action.selected },
+}));
 
 export default function IssueList() {
     const topics = useSelector((state: RootState) => state.bcf.topics);
     const selected = useSelector((state: RootState) => state.bcf.selectedTopic);
+    const dispatch = useDispatch();
 
-    const topicList = topics.map((input) => {
-        const topic = new BCF.THREE.Topic_Three();
-        topic.fromJSON(input);
-        const { uuid } = topic;
+    return (
+        <div style={{ height: '100%', width: '100%' }}>
+            <StyledDataGrid
+                rows={topics}
+                columns={columns}
+                initialState={{
+                    pagination: {
+                        paginationModel: { page: 0, pageSize: 5 },
+                    },
+                }}
+                pageSizeOptions={[5, 10]}
+                getRowId={(row) => row.uuid}
+                onRowClick={(params) => {
+                    // Find the topic from the state
+                    const topic = topics.filter((topic) => topic.uuid === params.id)[0];
+                    if (!topic) return;
 
-        return (
-            <IssueItem key={uuid} topic={topic} isSelected={selected?.uuid === uuid}>
-                {topic.direction}
-            </IssueItem>
-        );
-    });
+                    // Convert the topic to the THREE.js version
+                    const topic3 = new BCF.THREE.Topic_Three();
+                    topic3.fromJSON(topic);
+                    BCFViewer.setTopicCameraState(topic3);
 
-    return <div>{topicList}</div>;
+                    // Finally, update the state
+                    dispatch(selectTopic(topic.index));
+                }}
+                getCellClassName={(params: any) => {
+                    return params.id === selected?.uuid ? 'selected-row' : '';
+                }}
+            />
+        </div>
+    );
 }
