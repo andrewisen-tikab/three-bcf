cb742efd53520a68be42287f89a34c42fbafcbc1
André "MG" Wisén <andre.wisen@gmail.com>
2023-09-21 20:50:45 +0200

    Add `cypress`

---
+++ tests/cypress.config.ts
@@ -0,0 +1,10 @@
+import { defineConfig } from 'cypress';
+
+export default defineConfig({
+    e2e: {
+        baseUrl: 'http://localhost:5173/example/',
+        setupNodeEvents(on, config) {
+            // implement node event listeners here
+        },
+    },
+});
