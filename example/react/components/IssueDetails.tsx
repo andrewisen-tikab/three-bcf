f8aa5dbfc264d9f55dd280d85cf8938d72cf3175
André "MG" Wisén <andre.wisen@gmail.com>
2023-09-21 18:50:31 +0200

    Fix `example`

--- example/react/components/IssueDetails.tsx
+++ example/react/components/IssueDetails.tsx
@@ -18,23 +18,193 @@ import { TOPIC_STATUSES, TOPIC_TYPES } from '../../../src/core';
 import { TopicFolder_ThreeJSON } from '../../../src/types';
 import dayjs from 'dayjs';
 
-export default function IssueDetails() {
+function useUpdate() {
     const dispatch = useDispatch();
 
-    // TODO: Refactor `useSelector`
-    const disabled = useSelector((state: RootState) => state.bcf.selectedTopic == null);
-    const title = useSelector((state: RootState) => state.bcf.selectedTopic?.title);
-    const description = useSelector((state: RootState) => state.bcf.selectedTopic?.description);
-    const status = useSelector((state: RootState) => state.bcf.selectedTopic?.topicStatus);
-    const type = useSelector((state: RootState) => state.bcf.selectedTopic?.topicType);
-    const dueDate = useSelector((state: RootState) => state.bcf.selectedTopic?.dueDate);
-    const assignedTo = useSelector((state: RootState) => state.bcf.selectedTopic?.assignedTo);
-
-    if (disabled) return null;
-
-    const update = (key: keyof TopicFolder_ThreeJSON, value: string) => {
+    return (key: keyof TopicFolder_ThreeJSON, value: string) => {
         dispatch(updateTopic({ key, value }));
     };
+}
+
+function Title() {
+    const title = useSelector((state: RootState) => state.bcf.selectedTopic?.title);
+    const update = useUpdate();
+
+    return (
+        <TextField
+            label="Title"
+            value={title}
+            fullWidth
+            onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
+                update('title', event.target.value);
+            }}
+        />
+    );
+}
+
+function Description() {
+    const description = useSelector((state: RootState) => state.bcf.selectedTopic?.description);
+    const update = useUpdate();
+
+    return (
+        <TextField
+            label="Description"
+            value={description}
+            fullWidth
+            onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
+                update('description', event.target.value);
+            }}
+        />
+    );
+}
+
+function GeneralContainer() {
+    return (
+        <Grid container spacing={2}>
+            <Grid xs={12}>
+                <Title />
+                <Description />
+            </Grid>
+        </Grid>
+    );
+}
+
+function Status() {
+    const status = useSelector((state: RootState) => state.bcf.selectedTopic?.topicStatus);
+    const update = useUpdate();
+
+    return (
+        <FormControl fullWidth>
+            <InputLabel variant="standard" htmlFor="uncontrolled-native">
+                Status
+            </InputLabel>
+            <NativeSelect
+                value={status}
+                inputProps={{
+                    name: 'status',
+                    id: 'uncontrolled-native',
+                }}
+                onChange={(event: React.ChangeEvent<HTMLSelectElement>) => {
+                    update('topicStatus', event.target.value);
+                }}
+            >
+                {Object.values(TOPIC_STATUSES).map((key) => (
+                    <option key={key} value={key}>
+                        {key}
+                    </option>
+                ))}
+            </NativeSelect>
+        </FormControl>
+    );
+}
+
+function IssueType() {
+    const type = useSelector((state: RootState) => state.bcf.selectedTopic?.topicType);
+    const update = useUpdate();
+
+    return (
+        <FormControl fullWidth>
+            <InputLabel variant="standard" htmlFor="uncontrolled-native">
+                Issue Type
+            </InputLabel>
+            <NativeSelect
+                value={type}
+                inputProps={{
+                    name: 'type',
+                    id: 'uncontrolled-native',
+                }}
+                onChange={(event: React.ChangeEvent<HTMLSelectElement>) => {
+                    update('topicType', event.target.value);
+                }}
+            >
+                {Object.values(TOPIC_TYPES).map((key) => (
+                    <option key={key} value={key}>
+                        {key}
+                    </option>
+                ))}
+            </NativeSelect>
+        </FormControl>
+    );
+}
+
+function DueDate() {
+    const dueDate = useSelector((state: RootState) => state.bcf.selectedTopic?.dueDate);
+    const update = useUpdate();
+
+    return (
+        <FormControl fullWidth>
+            <DatePicker
+                label="Due date"
+                // @ts-ignore
+                value={dueDate === null ? null : dayjs(dueDate)}
+                // @ts-ignore
+                onChange={(newValue: Date) => {
+                    const newDate = dayjs(newValue).toString();
+                    update('dueDate', newDate);
+                }}
+                timezone="UTC"
+            />
+        </FormControl>
+    );
+}
+function CoordinationContainer() {
+    return (
+        <>
+            <Divider textAlign="left" role="presentation">
+                <Typography variant="subtitle1"> Coordination</Typography>
+            </Divider>
+            <Grid container spacing={2} sx={{ px: 1 }}>
+                <Grid xs={6}>
+                    <Status />
+                </Grid>
+                <Grid xs={6}>
+                    <IssueType />
+                </Grid>
+            </Grid>
+            <Grid container spacing={2}>
+                <Grid xs={12}>
+                    <DueDate />
+                </Grid>
+            </Grid>
+        </>
+    );
+}
+
+function AssignedTo() {
+    const assignedTo = useSelector((state: RootState) => state.bcf.selectedTopic?.assignedTo);
+    const update = useUpdate();
+
+    return (
+        <TextField
+            label="Assigned to"
+            value={assignedTo}
+            fullWidth
+            onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
+                update('assignedTo', event.target.value);
+            }}
+        />
+    );
+}
+
+function ResponsibilitiesContainer() {
+    return (
+        <>
+            <Divider textAlign="left" role="presentation">
+                <Typography variant="subtitle1"> Responsibilities </Typography>
+            </Divider>
+            <Grid container spacing={2}>
+                <Grid xs={12}>
+                    <AssignedTo />
+                </Grid>
+            </Grid>
+        </>
+    );
+}
+
+export default function IssueDetails() {
+    const isDisabled = useSelector((state: RootState) => state.bcf.selectedTopic == null);
+
+    if (isDisabled) return null;
 
     return (
         <CardContent>
@@ -46,105 +216,9 @@ export default function IssueDetails() {
                 noValidate
                 autoComplete="off"
             >
-                <TextField
-                    label="Title"
-                    value={title}
-                    fullWidth
-                    onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
-                        update('title', event.target.value);
-                    }}
-                />
-                <TextField
-                    label="Description"
-                    value={description}
-                    fullWidth
-                    onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
-                        update('description', event.target.value);
-                    }}
-                />
-                <Divider textAlign="left" role="presentation">
-                    <Typography variant="subtitle1"> Coordination</Typography>
-                </Divider>
-                <Grid container spacing={2} sx={{ px: 1 }}>
-                    <Grid xs={6}>
-                        <FormControl fullWidth>
-                            <InputLabel variant="standard" htmlFor="uncontrolled-native">
-                                Status
-                            </InputLabel>
-                            <NativeSelect
-                                value={status}
-                                inputProps={{
-                                    name: 'status',
-                                    id: 'uncontrolled-native',
-                                }}
-                                onChange={(event: React.ChangeEvent<HTMLSelectElement>) => {
-                                    update('topicStatus', event.target.value);
-                                }}
-                            >
-                                {Object.values(TOPIC_STATUSES).map((key) => (
-                                    <option key={key} value={key}>
-                                        {key}
-                                    </option>
-                                ))}
-                            </NativeSelect>
-                        </FormControl>
-                    </Grid>
-                    <Grid xs={6}>
-                        <FormControl fullWidth>
-                            <InputLabel variant="standard" htmlFor="uncontrolled-native">
-                                Issue Type
-                            </InputLabel>
-                            <NativeSelect
-                                value={type}
-                                inputProps={{
-                                    name: 'type',
-                                    id: 'uncontrolled-native',
-                                }}
-                                onChange={(event: React.ChangeEvent<HTMLSelectElement>) => {
-                                    update('topicType', event.target.value);
-                                }}
-                            >
-                                {Object.values(TOPIC_TYPES).map((key) => (
-                                    <option key={key} value={key}>
-                                        {key}
-                                    </option>
-                                ))}
-                            </NativeSelect>
-                        </FormControl>
-                    </Grid>
-                </Grid>
-                <Grid container spacing={2}>
-                    <Grid xs={12}>
-                        <FormControl fullWidth>
-                            <DatePicker
-                                label="Due date"
-                                // @ts-ignore
-                                value={dueDate === null ? null : dayjs(dueDate)}
-                                // @ts-ignore
-                                onChange={(newValue: Date) => {
-                                    const newDate = dayjs(newValue).toString();
-                                    update('dueDate', newDate);
-                                }}
-                                timezone="UTC"
-                            />
-                        </FormControl>
-                    </Grid>
-                </Grid>
-                <Divider textAlign="left" role="presentation">
-                    <Typography variant="subtitle1"> Responsibilities </Typography>
-                </Divider>
-                <Grid container spacing={2}>
-                    <Grid xs={12}>
-                        <TextField
-                            label="Assigned to"
-                            value={assignedTo}
-                            fullWidth
-                            onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
-                                update('assignedTo', event.target.value);
-                            }}
-                        />
-                    </Grid>
-                </Grid>
+                <GeneralContainer />
+                <CoordinationContainer />
+                <ResponsibilitiesContainer />
             </Box>
         </CardContent>
     );
